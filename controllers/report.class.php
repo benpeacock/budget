129f8715443cb2dd2f70d1a6f4e75270c6971010
benpeacock <benjaminwpeacock@gmail.com>
2013-09-09 02:19:46 +0000

    Providing export of reports from report.php to .csv

--- controllers/report.class.php
+++ controllers/report.class.php
@@ -38,5 +38,36 @@ class Report extends DatabaseObject {
 		}
 	}
 	
+		public function arrayToCsv(array &$array) {
+		   if (count($array) == 0) {
+		     return null;
+		   }
+		   ob_start();
+		   $fh = fopen("php://output", 'w');
+		   fputcsv($fh, array_keys(reset($array)));
+		   foreach ($array as $row) {
+		      fputcsv($fh, $row);
+		   }
+		   fclose($fh);
+		   return ob_get_clean();
+		}
+	
+		public function downloadSendHeaders($filename) {
+		    // disable caching
+		    $now = gmdate("D, d M Y H:i:s");
+		    header("Expires: Tue, 03 Jul 2001 06:00:00 GMT");
+		    header("Cache-Control: max-age=0, no-cache, must-revalidate, proxy-revalidate");
+		    header("Last-Modified: {$now} GMT");
+		
+		    // force download  
+		    header("Content-Type: application/force-download");
+		    header("Content-Type: application/octet-stream");
+		    header("Content-Type: application/download");
+		
+		    // disposition / encoding on response body
+		    header("Content-Disposition: attachment;filename={$filename}");
+		    header("Content-Transfer-Encoding: binary");
+		}
+		
 } // end report class
 
--- views/report.php
+++ views/report.php
@@ -1,7 +1,46 @@
 <?php require_once '../controllers/init.inc.php';
-require_once 'header.inc.php';
 
-if(isset($_POST['submit'])) {
+if(isset($_GET['action'])) {
+	$user_id = 1;
+	//$user_id = $_SESSION['user_id'];
+	$action = $_GET['action'];
+	
+	switch ($action) {
+		case 'download':
+			$budget = array();
+			$category = array();
+			$tag = array();
+			$include_overhead = false;
+			
+			if(isset($_POST['budget'])) { $budget = $_POST['budget']; }
+			if(isset($_POST['category'])) { $category = $_POST['category']; }
+			if(isset($_POST['tag'])) { $tag = $_POST['tag']; }
+			if(isset($_POST['include_overhead'])) { $include_overhead = $_POST['include_overhead']; }
+			
+			$report = new Report();
+			$result = $report->getItemReport($user_id, $budget, $category, $tag, $include_overhead);
+			foreach ($result as $row) {
+				$line = array();
+				$line[] = $row['budget'];
+				$line[] = $row['name'];
+				$line[] = $row['category'];
+				$line[] = $row['tag'];
+				$line[] = $row['amount'];
+				$download[] = $line;
+			}
+			if (empty($download)) {
+				echo 'No results available.';
+			} else {
+				$report->downloadSendHeaders("data_export_" . date("Y-m-d") . ".csv");
+				echo $report->arrayToCsv($download);
+				die();
+			}
+		break;
+	}
+	
+} //end GET actions
+
+if(isset($_POST['submit_html']) || isset($_POST['submit_csv'])) {
 	$user_id = 1;
 	// $user_id = $_SESSION['user_id'];
 	$budget = array();
@@ -18,22 +57,44 @@ if(isset($_POST['submit'])) {
 	if (empty($result)) {
 		$message = 'No results found.';
 	}
-	echo '<table>';
-	echo '<tbody>';
-	foreach ($result as $row) {
+	if (isset($_POST['submit_html'])) {
+		echo '<table>';
+		echo '<tbody>';
 		echo '<tr><th>Budget</th><th>Name</th><th>Category</th><th>Tag</th><th>Amount</th></tr>';
-		echo '<tr>';
-			echo '<td width="20%">' . $row['budget'] . '</td>';
-			echo '<td width="20%">' . $row['name'] . '</td>';
-			echo '<td width="20%">' . $row['category'] . '</td>';
-			echo '<td width="20%">' . $row['tag'] . '</td>';
-			echo '<td width="20%">' . $row['amount'] . '</td>';
-		echo '</tr>';
+		foreach ($result as $row) {
+			echo '<tr>';
+				echo '<td width="20%">' . $row['budget'] . '</td>';
+				echo '<td width="20%">' . $row['name'] . '</td>';
+				echo '<td width="20%">' . $row['category'] . '</td>';
+				echo '<td width="20%">' . $row['tag'] . '</td>';
+				echo '<td width="20%">' . $row['amount'] . '</td>';
+			echo '</tr>';
+		}
+		echo '<tr><td>Total</td><td></td><td></td><td></td><td>Goes here</td>';
+		echo '</tbody>';
+		echo '</table>';
 	}
-	echo '</tbody>';
-	echo '</table>';
+	if (isset($_POST['submit_csv'])) {
+		foreach ($result as $row) {
+			$line = array();
+			$line[] = $row['budget'];
+			$line[] = $row['name'];
+			$line[] = $row['category'];
+			$line[] = $row['tag'];
+			$line[] = $row['amount'];
+			$download[] = $line;
+		}
+		if (empty($download)) {
+			echo 'Could not write to csv.';
+		} else {
+			$report->downloadSendHeaders("data_export_" . date("Y-m-d") . ".csv");
+			echo $report->arrayToCsv($download);
+			die();
+		}
+	}
 }
 
+require_once 'header.inc.php';
 ?>
 <h2>Filters:</h2>
 <form action="report.php" method="post">
@@ -81,7 +142,8 @@ if(isset($_POST['submit'])) {
 			No: <input type="radio" name="include_overhead" value="no" checked="checked"/>
 		</li>
 		<li>
-			<input type="submit" name="submit" value="Create Report" />
+			<input type="submit" name="submit_html" value="Create Report" />
+			<input type="submit" name="submit_csv" value="Export Report to CSV" />
 		</li>
 	</ul>
 </form>
