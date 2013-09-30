9bd20932c984e6265a50f39d415228508483c15f
benpeacock <benjaminwpeacock@gmail.com>
2013-09-30 20:46:39 +0000

    Add total to html report table, add bootstrap styles

--- views/html_reports.php
+++ views/html_reports.php
@@ -1,9 +1,12 @@
 <div class="container">
-	<table>
+	<h2>Results</h2>
+	<table class="table table-striped">
 		<tbody>
 			<tr><th>Budget</th><th>Name</th><th>Category</th><th>Tag</th><th>Amount</th></tr>
 			<?php
+			$addup = array();
 			foreach ($report_result as $row) {
+				$addup[] = $row['amount'];
 				echo '<tr>';
 					echo '<td class="report-cell">' . $row['budget'] . '</td>';
 					echo '<td class="report-cell">' . $row['name'] . '</td>';
@@ -11,9 +14,10 @@
 					echo '<td class="report-cell">' . $row['tag'] . '</td>';
 					echo '<td class="report-cell">' . $row['amount'] . '</td>';
 				echo '</tr>';
+			$total= array_sum($addup);
 			}
 			?>
-			<tr><td>Total</td><td></td><td></td><td></td><td>Goes here</td>
+			<tr><td></td><td></td><td></td><td><strong><em>Total</em></strong></td><td><strong><em><?php echo $total; ?></em></strong></td>
 		</tbody>
 	</table>
 </div>
