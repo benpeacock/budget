92a0aa04724f62618a4bb38215bef89829c2d31c
benpeacock <benjaminwpeacock@gmail.com>
2013-10-02 21:12:42 +0000

    Fix dashboard displaying when user not logged in.  Creating dashboards view

--- controllers/dashboard.php
+++ controllers/dashboard.php
@@ -2,70 +2,12 @@
 require_once('../models/init.inc.php');
 require_once('../views/header.inc.php');
 
-if (!isset($session->user_id)) {
+if (isset($session->user_id)) {
+	include '../views/dashboards.php';
+} else {
 	include '../views/login_alert.php';
 }
+include '../views/footer.inc.php'; 
 ?>
-
-<div class="container">
-	<div id="budget_list" class="col-md-4">
-		<h2>My Budgets</h2>
-		<button type="button" class="btn btn-md" onclick="location.href='budget.php?action=create'">Create New Budget</button>
-		<ul>
-		<?php 
-		$budget = new Budget;
-		$records = $budget->getByUser($session->user_id);
-		
-		foreach ($records as $row) {
-			echo '<li><a href="budget.php?action=display&id=' . $row['id'] . '">' . $row['name'] . '</a></li>';
-		}
-		?>
-		</ul>
-	</div>
-		
-		<!-- <div id="overhead_item_list"> -->
-		<!-- <h2>My Overhead Items</h2> -->
-		<!-- <ul> -->
-		<?php
-		// $overhead_item = new OverheadItem();
-		// $records = $overhead_item->getByUser($user_id);
-		// foreach ($records as $row) {
-		// 	echo '<li><a href="overhead.php?action=display&id=' . $row['id'] . '">' . $row['name'] . '</a></li>';
-		// }
-		// ?>
-		<!-- <a href="overhead.php?action=create_item">+ Create New Overhead Item</a> -->
-		<!-- </ul> -->
-		<!-- </div> -->
-		
-	<div id="tag_list" class="col-md-4">
-		<h2>My Tags</h2>
-		<button type="button" class="btn btn-md" onclick="location.href='tag.php?action=create'">Create New Tag</button>
-		<ul>
-		<?php
-		$tag = new Tag;
-		$records = $tag->getByUser($session->user_id);
-		foreach ($records as $row) {
-			echo '<li><a href="tag.php?action=edit&id=' . $row['id'] . '">' . $row['name'] . '</a></li>';
-		}
-		?>
-		</ul>
-	</div>
-	<div id="category_list" class="col-md-4">
-		<h2>My Categories</h2>
-		<button type="button" class="btn btn-md" onclick="location.href='category.php?action=create'">Create New Category</button>
-		<ul>
-		<?php
-		$user_id = $_SESSION['user_id'];
-		$category = new Category;
-		$records = $category->getByUser($session->user_id);
-		foreach ($records as $row) {
-			echo '<li><a href="category.php?action=edit&id=' . $row['id'] . '">' . $row['name'] . '</a></li>';
-		}
-		?>
-		</ul>
-	</div>
-</div>
-
-<?php include '../views/footer.inc.php'; ?>
 
 
