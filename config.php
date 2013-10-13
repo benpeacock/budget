3478fddb3a7a139cbda6a455e8743d0a5ea3c538
benpeacock <benjaminwpeacock@gmail.com>
2013-10-13 22:28:13 +0000

    Changing file structure

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-const DB_USER = 'application';
-const DB_PASS = 'Torgy89*';
-
-//define('ROOT', '/var/www/accountabroad.com/public_html/');
-define('ROOT', dirname(__FILE__) . '/public_html/');
-
-?>
