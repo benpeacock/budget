6c1eab3e05c66f5ce8f083f03489c19cb88c76b4
benpeacock <benjaminwpeacock@gmail.com>
2013-10-20 14:30:08 +0000

    Using CDN for Jquery and Jquery UI

--- public_html/views/footer.inc.php
+++ public_html/views/footer.inc.php
@@ -7,16 +7,16 @@
         </footer>
         
 	</div><!-- ends container div -->
-        <!--  JQuery from CDN -->
-        <script src="/vendor/jquery-ui-1.10.3/jquery-1.9.1.js"></script>
-        <!--  JQuery UI from CDN -->
-        <script src="/vendor/jquery-ui-1.10.3/ui/1.10.3/jquery-ui.js"></script>
-        <!--  Twitter boostrap from CDN -->
+        <!--  JQuery -->
+        <script src="https://code.jquery.com/jquery-1.9.1.js"></script>
+        <!--  JQuery UI -->
+        <script src="https://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
+        <!--  Twitter boostrap -->
         <script src="/vendor/bootstrap-3.0.0/js/bootstrap.min.js"></script>
-        <!--  X-editable plugin from CDN -->
+        <!--  X-editable plugin -->
         <script src="/vendor/bootstrap3-editable/js/bootstrap-editable.min.js"></script>
         <script src="/js/editable.js"></script>
-        <!--  MultiSelect JQuery plugin from local file -->
+        <!--  MultiSelect JQuery plugin -->
         <script src="/js/jquery.multiselect.js"></script>
         <script src="/js/multiselect_control.js"></script>
 	</body>
